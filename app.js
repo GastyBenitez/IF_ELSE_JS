7340e835e3f240673110964e4524191f7730b893
gaston benitez <gastonbenitez0207@gmail.com>
2021-12-09 21:42:17 -0300

    cadenas de texto del 1 al 10

---
+++ app.js
@@ -0,0 +1,87 @@
+// Ejercicio 1
+//const texto = prompt("Ingrese un texto");
+//if(texto.includes("a") || texto.includes("A")){
+    //alert("tu texto contiene la letra A");
+//}else{
+    //alert("Tu texto NO contiene la letra A");
+//}
+
+
+
+
+
+
+// Ejercicio 2
+//const texto = prompt("Ingrese un texto");
+//alert(texto.toLowerCase());
+//alert(texto.toUpperCase());
+
+//Ejercicio 3
+//const texto = prompt("Ingrese un texto");
+//alert("Tu texto comienza con la letra " " "texto[0]);
+
+// Ejercicio 4
+//const texto = prompt("Ingrese un texto");
+//alert(texto.length);
+
+//Ejercicio 5
+//const numero = parseInt(prompt("Ingrese un número"));
+//alert(numero.toString().length);
+
+
+//Ejercicio 6
+//const texto = prompt("Ingrese un texto");
+//alert("Tu texto termina con " + " " + texto.slice(-1));
+
+
+//Ejercicio 7
+//const texto = prompt("Ingrese un oración");
+//alert(texto.split(" ").join("*"));
+
+
+//Ejercicio 8
+//const letra = prompt("Ingrese una letra");
+//const texto = prompt("Ingrese un texto");
+//if(texto.includes(letra) || texto.includes(letra.toUpperCase()) || texto.includes(letra.toLowerCase()))
+//{
+
+    //alert("tu texto contiene la letra " + " " + letra);
+//}else{
+    //alert("Tu texto NO contiene la letra " + " " + letra);
+//}
+    
+
+//Ejercicio 9
+//const texto = prompt("Ingrese un texto");
+//alert(texto.slice(1));
+
+
+//Ejercicio 10
+//const nombre = prompt("Ingrese un nombre");
+//alert((nombre[0].toUpperCase()).concat(nombre.slice(1)));
+
+//CONDICIONALES 
+
+//Agregar al ejercicio 5 de Variables un validador de datos. Si ingresa un número entonces que me calcule las medidas, sino, que muestre un mensaje de error. 
+
+//Ejercicio 1
+
+const medidaInicialMetros = parseFloat(prompt("Por favor ingrese una medida en METROS"));
+
+const medidaEnPies = medidaInicialMetros * 3.28;
+const medidaEnCentimetros = medidaInicialMetros * 100;
+const medidaEnPulgadas = medidaInicialMetros * 39.3701;
+
+if 
+
+alert("la medida en pies es: " + medidaEnPies);
+alert("la medida en pies es: " + medidaEnCentimetros);
+alert("la medida en pies es: " + medidaEnPulgadas);
+
+
+
+
+
+
+
+
